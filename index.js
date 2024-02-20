5027e9f0a4a0444bd99d42a710392432b6e0bc9b
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-20 15:07:25 +0530

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 const express = require('express');
 const request = require('request-promise');
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+const returnScraperApiUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the API' });
+    res.send('Welcome to the Amazon Scraper API');
+});
+
+// Get product details
+app.get('/products/:productId', async (req, res) => {
+    const { productId } = req.params;
+    const { api_key } = req.query;
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+// Get product reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+// Get product offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+app.get('/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params;
+    const { api_key } = req.query;
+
+    try {
+        const response = await request(`${returnScraperApiUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
 });
 
 app.listen(PORT, () => {
